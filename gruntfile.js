04488b638b92addfb6c48d1491fafde2e80b4677
Eric Damtoft <edamtoft@gmail.com>
2015-08-09 13:29:26 -0400

    Build Improvements
    Also made ready for npm registry

--- gruntfile.js
+++ gruntfile.js
@@ -3,17 +3,23 @@ module.exports = function(grunt){
 		pkg: grunt.file.readJSON('package.json'),
     
     jshint: {
-      files: ['src/<%= pkg.name %>.js']
+      files: ['src/*.js']
     },
     
     nodeunit: {
-      all: ['tests/<%= pkg.name %>-tests.js'],
+      all: ['tests/*-tests.js'],
     },
     
+    copy: {
+      default: {
+        src: 'src/<%= pkg.name %>.js',
+        dest: 'dist/<%= pkg.name %>.js'
+      }
+    },
     
 		uglify: {
 		  default: {
-		    src: 'src/<%= pkg.name %>.js',
+		    src: 'dist/<%= pkg.name %>.js',
 		    dest: 'dist/<%= pkg.name %>.min.js'
 		  }
 		}
@@ -21,7 +27,8 @@ module.exports = function(grunt){
 	
 	grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-uglify');
 	
-	grunt.registerTask('default', ['jshint','nodeunit','uglify']);
+	grunt.registerTask('default', ['jshint','nodeunit','copy','uglify']);
 }
